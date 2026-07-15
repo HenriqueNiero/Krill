8f96ab1a64bc43af0f875ab68099b3f7f1bba9c4
HenriqueNiero <henriqueniero@B-FMR13-W.abtlus.org.br>
2026-07-15 15:38:10 -0300

    prepareFasta.py modificacao no rename dos arquivos, salvando em nova tabela cabecalhos de contigs

--- Krill/prepareFastas.py
+++ Krill/prepareFastas.py
@@ -21,25 +21,45 @@ def run(path,ext):
 
 		#get info and generate metadata database
 		base = []
+		df_contigs_header = pd.DataFrame({'original_filename': [], 'new_filename': [], 'original_contigs': [], 'new_contigs': []})
 		count = 1
-		for f in fastas:
+		for f in fastas:			
+			# print(f)
 			basename = str(os.path.basename(f))
 			new_name = f'{count:09d}.{ext}'
 			os.rename(basename, new_name)
 			base.append([basename,  new_name])
+			# Define your pattern and file path
+			# Run the awk command: awk '/pattern/ {print}' 
+			# Split the output by lines and clean up any trailing empty lines	
+			fileresult = subprocess.run(["awk", f"/^>/", new_name], capture_output=True, text=True)
+			original_contigs = [line[1:] for line in fileresult.stdout.splitlines() if line]
+			# print(original_contigs)
 			cmd_rename_fasta = '''gawk -i inplace '/^>/{print ">" substr(FILENAME,1,length(FILENAME)-%i); next} 1' %s''' % (len(ext)+1,new_name)
 			cmd_rename_fasta_headers = f"""awk '{{if (/^>/) print ">"substr($0,2)"_"(++i); else print $0;}}' {new_name} > tmp && mv tmp {new_name}"""
 			subprocess.run(cmd_rename_fasta, shell=True, executable='/bin/bash')
 			subprocess.run(cmd_rename_fasta_headers, shell=True, executable='/bin/bash')
+			# Split the output by lines and clean up any trailing empty lines	
+			fileresult = subprocess.run(["awk", f"/^>/", new_name], capture_output=True, text=True)
+			new_contigs = [line[1:] for line in fileresult.stdout.splitlines() if line]
+			# print(new_contigs)
+			df = pd.DataFrame({'original_filename': basename, 'new_filename': new_name, 'original_contigs': original_contigs, 'new_contigs': new_contigs})
+			df_contigs_header = pd.concat([df_contigs_header, df])
 			count += 1
+
+
 
 		df = pd.DataFrame(base, columns=['OriginalName', 'NewName'])
 		df.to_csv(os.path.join(path,'fastaFilesRenamed.tsv'),sep='\t',index=False)
 		cprint.info('# Changed metadata saved in {}'.format(os.path.join(path,'fastaFilesRenamed.tsv')))
+
+		df_contigs_header.to_csv(os.path.join(path,'contigsFilesRenamed.tsv'),sep='\t',index=False)
+		cprint.info('# Changed contig metadata saved in {}'.format(os.path.join(path,'contigsFilesRenamed.tsv')))
 	else:
 		cprint.warn('Files already prepared.')
 
 
 if __name__ == '__main__':
 	path = sys.argv[1]
+	# run(path,"fasta")
 	convert2fasta(path)
